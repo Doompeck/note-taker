2b7b657759392c2643b85ce204198f194fc532c0
Benjamin <bstanf@uw.edu>
2022-10-02 19:57:33 -0700

    forgot a syntax error

--- db/store.js
+++ db/store.js
@@ -2,7 +2,7 @@ const util = require("util");
 const fs = require("fs");
 const uuid = require("uuid").v1;
 
-const readFileAsync = util.primisify(fs.readFile);
+const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
