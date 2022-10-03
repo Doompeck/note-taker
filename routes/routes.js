b93f78fe6059ffde15bde870af6ade2d3dca5441
Benjamin <bstanf@uw.edu>
2022-10-02 19:54:21 -0700

    updated for new syntax.  cross fingers

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,11 @@
 // Requires
 const router = require('express').Router();
 const store = require('../db/store');
-
-
+const path = require('path');
 
 // API routes
 // Note get route to return all notes.
-    app.get("/notes", (req, res) => {
+    router.get("/notes", (req, res) => {
         store
             .getNotes()
             .then(notes => {
@@ -18,37 +17,31 @@ const store = require('../db/store');
     });
 
 // Note post route for new note.
-    app.post("/api/notes", function(req, res) {
-        let newNote = req.body;
-        notes.push(newNote);
-        updateDB();
-        return console.log("New note added: " + newNote.title);
+    router.post("/notes", (req, res) => {
+        store
+            .addNote(req.body)
+            .then(note => {
+                res.json(note);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+    });
+    
+// Delete a post by id.
+    router.delete("/notes/:id", (req, res) => {
+// ?????
     });
 
-// Not get route for individual notes with the id in the parameter.
-    app.get("/api/notes/:id", function(req, res) {
-        req.json(notes[req.params.id]);
+// Send notes to notes.html
+    router.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
-    app.delete("/api/notes/:id", function(req, res) {
-        notes.splice(req.params.id, 1);
-        updateDB();
-        console.log("Note deleted with the following ID: " +req.params.id);
-    });
 
-    // Display notes.html route
-    app.get('*', function(req, res) {
+// Send to homepage in case of problem
+    router.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-
-// This function is used to update the json file when notes are added or deleted.
-    function updateDB() {
-        fs.writeFile("db/db.json", JSON.stringify(note, '\t'),err => {
-            if (err) throw err;
-            return true;
-        });
-    };
-
-
 
 module.exports = router;
